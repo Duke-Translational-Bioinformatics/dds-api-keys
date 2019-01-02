122bbbbbda5e3e1ff3770f451cb5d3124c29d2f3
Darin London <darin.london@duke.edu>
2019-01-02 08:45:27 -0500

    removed unnecessary layer of indirection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-import index from "./js/index"
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from 'react-redux'
+import store from './js/store'
+import App from "./js/components/presentational/App";
+// uncomment next line to see store in the browser console
+// window.store = store;
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
