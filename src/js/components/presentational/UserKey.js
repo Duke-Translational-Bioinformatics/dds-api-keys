9e83b0c1ffcc6d41d64cdfa17d587121d41dab35
Darin London <darin.london@duke.edu>
2019-01-03 13:17:06 -0500

    replaced place holder createNewUser with authenticateUser - uses the authHelper.login() function with a promise - added handlers for authHelper.login() success and failure - added rudimentary logic to notify user that their user is loading during the login process

--- src/js/components/presentational/UserKey.js
+++ src/js/components/presentational/UserKey.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import PropTypes from 'prop-types'
 import ManageKey from "./ManageKey"
-import uuidv1 from "uuid";
+import authHelper from '../../helpers/authHelper';
+import ddsClient from '../../helpers/ddsClient';
 
 class UserKey extends Component {
   constructor(props) {
@@ -9,11 +10,14 @@ class UserKey extends Component {
     this.confirmApiKeyDeletion = this.confirmApiKeyDeletion.bind(this);
     this.confirmApiKeyRegeneration = this.confirmApiKeyRegeneration.bind(this);
     this.generateUserApiKey = this.generateUserApiKey.bind(this);
+    this.authenticateUser = this.authenticateUser.bind(this);
+    this.handleAuthenticationSuccess = this.handleAuthenticationSuccess.bind(this);
+    this.handleCurrentUser = this.handleCurrentUser.bind(this);
   }
 
   componentDidMount() {
     if (this.props.currentUser == null) {
-      this.newCurrentUser();
+      this.authenticateUser();
     }
   }
 
@@ -41,22 +45,49 @@ class UserKey extends Component {
     this.props.setUserApiKey(newKey)
   }
 
-  newCurrentUser() {
-    const newU = uuidv1();
-    this.props.setCurrentUser(newU)
+  authenticateUser() {
+    authHelper.login().then(
+      this.handleAuthenticationSuccess,
+      this.handleException
+    );
+  }
+
+  handleAuthenticationSuccess(isSuccessful) {
+    var jwtToken = authHelper.jwt();
+    var currentUser = ddsClient.getCurrentUser(
+      jwtToken,
+      this.handleCurrentUser,
+      this.handleException
+    )
+  }
+
+  handleException(errorMessage) {
+    alert(JSON.stringify(errorMessage));
+  }
+
+  handleCurrentUser(user) {
+    this.props.setCurrentUser(user);
   }
 
   render() {
+    let userDisplay;
     let keyDisplay;
-    if(this.props.userApiKey == null) {
-      keyDisplay = <button onClick={this.generateUserApiKey}>Generate Key</button>
+    if(this.props.currentUser == null) {
+      userDisplay = <p>Fetching User</p>;
+      keyDisplay = <p></p>
     }
     else {
-      keyDisplay = <ManageKey destroyKey={this.confirmApiKeyDeletion} regenerateKey={this.confirmApiKeyRegeneration} apiKey={this.props.userApiKey} />
+      userDisplay = <p>User { this.props.currentUser.full_name }</p>
+      if(this.props.userApiKey == null) {
+        keyDisplay = <button onClick={this.generateUserApiKey}>Generate Key</button>
+      }
+      else {
+        keyDisplay = <ManageKey destroyKey={this.confirmApiKeyDeletion} regenerateKey={this.confirmApiKeyRegeneration} apiKey={this.props.userApiKey} />
+      }
     }
     return (
       <div>
-        <p>User { this.props.currentUser } Key { this.props.userApiKey }</p>
+        {userDisplay}
         {keyDisplay}
       </div>
     )
@@ -65,7 +96,7 @@ class UserKey extends Component {
 
 UserKey.propTypes = {
   userApiKey: PropTypes.string,
-  currentUser: PropTypes.string,
+  currentUser: PropTypes.object,
   setCurrentUser: PropTypes.func.isRequired,
   setUserApiKey: PropTypes.func.isRequired,
   destroyUserApiKey: PropTypes.func.isRequired
