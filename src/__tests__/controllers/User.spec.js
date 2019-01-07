f4da2b42a057a2854ab1b69a89f526a7bd96d157
Darin London <darin.london@duke.edu>
2019-01-07 15:14:11 -0500

    learned how to import Enzyme and a specific exported item in the same line

--- src/__tests__/controllers/User.spec.js
+++ src/__tests__/controllers/User.spec.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Enzyme from 'enzyme';
-import { shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import User from 'js/controllers/User'
 import configureMockStore from 'redux-mock-store';
