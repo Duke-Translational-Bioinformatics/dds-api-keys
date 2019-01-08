9d634303d2bd9a7aff23cb72b50f936dd5be6407
Darin London <darin.london@duke.edu>
2019-01-08 10:00:07 -0500

    asserts that App renders a User

---
+++ src/__tests__/views/App.spec.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../enzymeConfig';
+import { shallow } from 'enzyme';
+
+import App from 'js/views/App'
+import User from "js/controllers/User";
+
+describe('App', () => {
+  it("renders a div with a User", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(User)).toExist();
+  });
+});
