d6d43858839efd163bdbd4ca92c1fbb2244e90fc
Darin London <darin.london@duke.edu>
2019-01-16 10:33:15 -0500

    express application: - serves on 8080 - logs to stdout - serves static files built into dist

---
+++ app.js
@@ -0,0 +1,28 @@
+var path = require("path");
+var express = require("express");
+var app = express();
+var helmet = require("helmet");
+var winston = require("winston");
+const port = 8080;
+
+// log everything to stdout for use in kubernetes
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console({
+      handleExceptions: true
+    })
+  ],
+  exitOnError: false
+});
+
+console.log("NODE ENV IS:", process.env.NODE_ENV);
+app.use(helmet());
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("/*", function(req, res) {
+  res.render("index");
+});
+
+app.listen(port, function() {
+  logger.info(`dds-api-keys listening on port ${port}!`);
+});
